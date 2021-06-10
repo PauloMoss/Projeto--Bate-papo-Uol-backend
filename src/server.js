10837486facfebdd11b98b69a212555b804c5b2d
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-06-10 18:26:42 -0300

    Implementacao login usuario

--- src/server.js
+++ src/server.js
@@ -7,8 +7,25 @@ app.use(express.json());
 app.use(cors());
 
 fs.existsSync("./data.json");
-const allData = fs.readFileSync("./data.json",'utf8')
-let data = JSON.parse(allData).teste;
+const data = fs.readFileSync("./data.json",'utf8')
+const participants = JSON.parse(data).participants;
+const messages = JSON.parse(data).messages;
+
+app.post("/participants", (req, res) => {
+    
+    const newUser = req.body;
+
+    if(newUser.name.length === 0) {
+        res.sendStatus(400);
+        return;
+    }
+    const participant = {...newUser, lastStatus: Date.now()}
+    const message = {from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:MM:SS'}
+    participants.push(participant)
+    messagens.push(message)
+    res.sendStatus(200)
+    fs.writeFileSync("./data.json", JSON.stringify({participants, messages}));
+})
 
 
 
